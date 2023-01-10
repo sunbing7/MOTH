162444f527f2f38f8f5447f4cf6f8074eeb04618
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-01-10 10:40:24 +0800

    test

--- src_torch/util.py
+++ src_torch/util.py
@@ -145,15 +145,15 @@ def get_model(args):
 def pgd_attack(model, images, labels, mean, std,
                eps=0.3, alpha=2/255, iters=40):
     loss = torch.nn.CrossEntropyLoss()
-
+    print('DEBUG images shape:{}'.format(images.shape))
     ori_images = images.data
-
+    print('DEBUG ori_images shape:{}'.format(ori_images.shape))
     images = images + 2 * (torch.rand_like(images) - 0.5) * eps
     images = torch.clamp(images, 0, 1)
 
     mean = mean.to(images.device)
     std  = std.to(images.device)
-    print('DEBUG images shape:{}'.format(images.shape))
+
     for i in range(iters):
         images.requires_grad = True
 
