5b283a8490d58c75656e70a0dbe42753567e98da
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-15 16:07:27 +0800

    lr

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 
 
+
+
+#Z
+python src_torch/main.py --phase=moth --suffix=Z_adv --lr=0.00001 --dataset=asl --model=MobileNet --type=adv --batch_size=32 --epochs=2 --data_ratio=0.05
+
+
+#caltech
+#g_kan
+python src_torch/main.py --phase=moth --suffix=g_kan_adv --lr=0.0000001 --dataset=caltech --model=shufflenetv2 --type=adv --batch_size=64 --epochs=2
+
 #gtsrb
 #dtl
 python src_torch/main.py --phase=moth --suffix=dtl_adv --lr=0.00000001 --dataset=gtsrb --model=vgg11_bn --type=adv --batch_size=128 --epochs=2
@@ -10,24 +20,15 @@ python src_torch/main.py --phase=moth --suffix=dtl_adv --lr=0.000000001 --datase
 python src_torch/main.py --phase=moth --suffix=dkl_adv --lr=0.00000001 --dataset=gtsrb --model=vgg11_bn --type=adv --batch_size=128 --epochs=2
 python src_torch/main.py --phase=moth --suffix=dkl_adv --lr=0.000000001 --dataset=gtsrb --model=vgg11_bn --type=adv --batch_size=128 --epochs=2
 
+python src_torch/main.py --phase=moth --suffix=g_kan_adv --lr=0.000001 --dataset=caltech --model=shufflenetv2 --type=adv --batch_size=64 --epochs=2
+#brain
+python src_torch/main.py --phase=moth --suffix=brain_adv --lr=0.000001 --dataset=caltech --model=shufflenetv2 --type=adv --batch_size=64 --epochs=2
+
 
 #asl
 #A
 
 python src_torch/main.py --phase=moth --suffix=A_adv --lr=0.00001 --dataset=asl --model=MobileNet --type=adv --batch_size=32 --epochs=2 --data_ratio=0.05
-
-#Z
-python src_torch/main.py --phase=moth --suffix=Z_adv --lr=0.00001 --dataset=asl --model=MobileNet --type=adv --batch_size=32 --epochs=2 --data_ratio=0.05
-
-
-#caltech
-#brain
-python src_torch/main.py --phase=moth --suffix=brain_adv --lr=0.00000001 --dataset=caltech --model=shufflenetv2 --type=adv --batch_size=64 --epochs=2
-python src_torch/main.py --phase=moth --suffix=brain_adv --lr=0.000001 --dataset=caltech --model=shufflenetv2 --type=adv --batch_size=64 --epochs=2
-
-#g_kan
-python src_torch/main.py --phase=moth --suffix=g_kan_adv --lr=0.00000001 --dataset=caltech --model=shufflenetv2 --type=adv --batch_size=64 --epochs=2
-python src_torch/main.py --phase=moth --suffix=g_kan_adv --lr=0.000001 --dataset=caltech --model=shufflenetv2 --type=adv --batch_size=64 --epochs=2
 
 
 
